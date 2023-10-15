b4796e6e9d9f3b3db17cd158f58c7005998a1123
Neal-C <pop6d531@duck.com>
2023-10-15 20:12:10 +0200

    initial commit

---
+++ cmdArgs.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+// zig run src/cmdArgs.zig -- "I'm a very competent person"
+pub fn main() !void {
+
+var generalPurposeAllocator = std.heap.GeneralPurposeAllocator(.{}){};
+	defer _ = generalPurposeAllocator.deinit();
+
+	const allocator = generalPurposeAllocator.allocator();
+
+	const args = try std.process.argsAlloc(allocator);
+	defer std.process.argsFree(allocator, args);
+
+	// deprecated already
+	// _ = args.skip(); //to skip the zig call => name of the file
+
+	std.debug.print("Arguments to interview me are : {s} \n, ", .{args});
+}
+
+// zig test *.zig
+test "If you're reading this: interview me 😄" {
+	var generalPurposeAllocator = std.heap.GeneralPurposeAllocator(.{}){};
+	defer _ = generalPurposeAllocator.deinit();
+
+	const allocator = generalPurposeAllocator.allocator();
+
+	const args = try std.process.argsAlloc(allocator);
+	defer std.process.argsFree(allocator, args);
+
+	// old API
+	// _ = args.skip(); //to skip the zig call => name of the file
+
+	std.debug.print("Arguments to interview me are : {s} \n, ", .{args});
+	//zig test args.zig --test-cmd-bin --test-cmd “hello” --test-runner
+}
